4ccdb479b061c9ac07223e4f3dbfb6ede9f820d5
Pierre-Henry Soria <hi@ph7.me>
2022-07-17 20:30:04 +0930

    Show nice error message when no users on homepage

--- src/pages/user/UsersList.jsx
+++ src/pages/user/UsersList.jsx
@@ -5,12 +5,23 @@ import { Button, Card, Col, Row } from "react-bootstrap";
 import { NavLink } from "react-router-dom";
 
 const UsersList = () => {
-  const [users, setUsers] = useState({}); // important, default need to be empty object
+  const [users, setUsers] = useState({}); // Important, default need to be empty object
+  const [errorMessage, setErrorMessage] = useState(null);
 
   const fetchUsers = async () => {
-    const users = await userService.retrieveAllUsers();
+    try {
+      const users = await userService.retrieveAllUsers();
+      setUsers(users);
+    } catch (error) {
+      const retrieveErrorMessage = () => {
+        const {
+          data: { message },
+        } = error.response;
 
-    setUsers(users);
+        return message;
+      };
+      setErrorMessage(retrieveErrorMessage());
+    }
   };
 
   useEffect(() => {
@@ -19,31 +30,37 @@ const UsersList = () => {
 
   return (
     <Layout>
-      <h3 className="text-center mb-3">Users</h3>
-      {Object.values(users).map((user) => (
-        <Row key={user.id} className="justify-content-center">
-          <Col lg={4}>
-            <Card>
-              <Card.Body>
-                <h4>{user.name}</h4>
-                <p>{user.email}</p>
-                {user.city && user.country && (
-                  <p>
-                    {user.city} - {user.country}
-                  </p>
-                )}
-                <Button
-                  variant="secondary"
-                  as={NavLink}
-                  to={`/edit/${user.id}`}
-                >
-                  Edit User
-                </Button>
-              </Card.Body>
-            </Card>
-          </Col>
-        </Row>
-      ))}
+      {errorMessage ? (
+        <h3 className="text-center text-danger fw-bold">{errorMessage}</h3>
+      ) : (
+        <>
+          <h3 className="text-center mb-3">Users</h3>
+          {Object.values(users).map((user) => (
+            <Row key={user.id} className="justify-content-center">
+              <Col lg={4}>
+                <Card>
+                  <Card.Body>
+                    <h4>{user.name}</h4>
+                    <p>{user.email}</p>
+                    {user.city && user.country && (
+                      <p>
+                        {user.city} - {user.country}
+                      </p>
+                    )}
+                    <Button
+                      variant="secondary"
+                      as={NavLink}
+                      to={`/edit/${user.id}`}
+                    >
+                      Edit User
+                    </Button>
+                  </Card.Body>
+                </Card>
+              </Col>
+            </Row>
+          ))}
+        </>
+      )}
     </Layout>
   );
 };
