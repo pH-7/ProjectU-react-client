a234abd03138d9fac45740b99c8fae42a2c7100c
Pierre-Henry Soria <hi@ph7.me>
2022-07-28 12:48:09 +0930

    [Consistency] Change colour to HEX `#ffffff`

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,23 +1,23 @@
-import Footer from "./Footer";
-import TopNavigation from "./TopNavigation";
-import { Container } from "react-bootstrap";
-import { ToastContainer } from "react-toastify";
-import { createGlobalStyle } from "styled-components";
+import Footer from './Footer';
+import TopNavigation from './TopNavigation';
+import { Container } from 'react-bootstrap';
+import { ToastContainer } from 'react-toastify';
+import { createGlobalStyle } from 'styled-components';
 
 const BackgroundColor = createGlobalStyle`
     body {
-        background-color: ${(props) => (props?.light ? "#f2f2f2" : "white")};
+        background-color: ${(props) => (props?.light ? '#f2f2f2' : '#ffffff')};
     }
 `;
 
 const Layout = ({ children }) => {
   return (
     <>
-      <Container fluid className="mb-5">
+      <Container fluid className='mb-5'>
         <BackgroundColor light />
         <TopNavigation />
         <ToastContainer />
-        <Container className="mt-5">{children}</Container>
+        <Container className='mt-5'>{children}</Container>
       </Container>
       <Footer />
     </>
