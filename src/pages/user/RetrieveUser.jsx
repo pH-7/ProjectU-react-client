f9d156ef308ef7271b184bc92f09165e480822f4
♚ PH⑦ de Soria™♛ <hi@ph7.me>
2023-01-20 20:47:48 +1100

    Resort imports

--- src/pages/user/RetrieveUser.jsx
+++ src/pages/user/RetrieveUser.jsx
@@ -1,9 +1,10 @@
-import Layout from "../../components/layout/Layout";
-import * as userService from "../../services/user.service";
 import React from "react";
 import { useEffect, useState } from "react";
 import { Card, Col, Row } from "react-bootstrap";
 import { useParams } from "react-router-dom";
+
+import Layout from "../../components/layout/Layout";
+import * as userService from "../../services/user.service";
 
 const RetrieveUser = () => {
   const { userId } = useParams();
