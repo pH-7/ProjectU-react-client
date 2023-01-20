d8015ae34e3317cb2ee5fd7ff9e8484dc261a55d
♚ PH⑦ de Soria™♛ <hi@ph7.me>
2023-01-20 20:48:34 +1100

    Resort imports

--- src/pages/user/CreateUser.jsx
+++ src/pages/user/CreateUser.jsx
@@ -1,9 +1,10 @@
-import Layout from "../../components/layout/Layout";
-import { firstUpperCase } from "../../helpers/string.helper";
-import * as userService from "../../services/user.service";
 import React, { useState } from "react";
 import { Button, Col, Form, Row } from "react-bootstrap";
 import { toast } from "react-toastify";
+
+import Layout from "../../components/layout/Layout";
+import { firstUpperCase } from "../../helpers/string.helper";
+import * as userService from "../../services/user.service";
 
 const CreateUser = () => {
   const [name, setName] = useState("");
